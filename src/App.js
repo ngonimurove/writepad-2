5967fd8f12cb3dbdf8865b0937665d4337240fdc
Taps Murove <taps.murove@gmail.com>
2017-07-31 21:52:25 +0200

    Add Semantic UI and basic text editing
    Added semantic UI components and text editing features

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 // Import React!
-import React from 'react'
-import { Editor, Raw } from 'slate'
+import './App.css';
+import keycode from 'keycode';
+import React from 'react';
+import { Container, Card } from 'semantic-ui-react'
+import { Editor, Raw } from 'slate';
 
-const initialState = Raw.deserialize({
-  nodes: [
-    {
-      kind: 'block',
-      type: 'paragraph',
-      nodes: [
-        {
-          kind: 'text',
-          text: 'A line of text in a paragraph.'
-        }
-      ]
+// Update the initial content to be pulled from Local Storage if it exists.
+const initialContent = (
+  JSON.parse(localStorage.getItem('content')) ||
+  {
+    nodes: [
+      {
+        kind: 'block',
+        type: 'paragraph'
+      }
+    ]
+  }
+)
+
+function MarkHotkey(options) {
+  const { type, key, isAltKey = false } = options
+
+  // Return our "plugin" object, containing the `onKeyDown` handler.
+  return {
+    onKeyDown(event, data, state) {
+      // Check that the key pressed matches our `code` option.
+      if (!event.metaKey || keycode(event.which) !== key || event.altKey !== isAltKey) return
+
+      // Prevent the default characters from being inserted.
+      event.preventDefault()
+
+      // Toggle the mark `type`.
+      return state
+        .transform()
+        .toggleMark(type)
+        .apply()
     }
-  ]
-}, { terse: true })
+  }
+}
+
+const plugins = [
+  MarkHotkey({ key: 'b', type: 'bold' }),
+  MarkHotkey({ key: 'c', type: 'code', isAltKey: true }),
+  MarkHotkey({ key: 'i', type: 'italic' }),
+  MarkHotkey({ key: 'd', type: 'strikethrough' }),
+  MarkHotkey({ key: 'u', type: 'underline' })
+]
 
 // Define our app...
 class App extends React.Component {
 
   // Set the initial state when the app is first constructed.
   state = {
-    state: initialState
+    state: Raw.deserialize(initialContent, {terse: true}),
+    schema: {
+      nodes: {
+        paragraph: props => <Card fluid {...props.attributes}><Card.Content>{props.children}</Card.Content></Card>
+      },
+      marks: {
+        bold: props => <strong>{props.children}</strong>,
+        code: props => <code>{props.children}</code>,
+        italic: props => <em>{props.children}</em>,
+        strikethrough: props => <del>{props.children}</del>,
+        underline: props => <u>{props.children}</u>,
+      }
+    }
   }
 
   // On change, update the app's React state with the new editor state.
@@ -30,13 +72,27 @@ class App extends React.Component {
     this.setState({ state })
   }
 
+  onDocumentChange = (document, state) => {
+    // Switch to using the Raw serializer.
+    const content = JSON.stringify(Raw.serialize(state))
+    localStorage.setItem('content', content)
+  }
+
   // Render the editor.
   render = () => {
     return (
-      <Editor
-        state={this.state.state}
-        onChange={this.onChange}
-      />
+      <Container fluid
+        className="notepad"
+      >
+        <Editor
+          plugins={plugins}
+          schema={this.state.schema}
+          state={this.state.state}
+          onChange={this.onChange}
+          onDocumentChange={this.onDocumentChange}
+          onKeyDown={this.onKeyDown}
+        />
+      </Container>
     )
   }
 
