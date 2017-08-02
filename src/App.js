7af01516cda7babda96fb90ef0899368caf87d8c
Taps Murove <taps.murove@gmail.com>
2017-08-02 23:30:00 +0200

    Update context menu
    Additional menu items were added to the context menu.
    Each menu item also now has a popup label next to it

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import keycode from 'keycode';
 import React from 'react';
-import { Container, Segment, Header, Sidebar, Menu, Icon, List } from 'semantic-ui-react'
+import { Container, Segment, Header, Sidebar, Menu, Icon, List, Popup } from 'semantic-ui-react'
 import { Editor, Raw } from 'slate';
 
 // Update the initial content to be pulled from Local Storage if it exists.
@@ -55,10 +55,12 @@ class EditorContainer extends React.Component {
     state: Raw.deserialize(initialContent, {terse: true}),
     schema: {
       nodes: {
-        paragraph: props => <Segment {...props.attributes}>{props.children}</Segment>,
-        section: props => <Segment {...props.attributes}>{props.children}</Segment>, // A section always has a heading at the top
-        header: props => <Header as='h3' block {...props.attributes}>{props.children}</Header>,
-        group: props => <Segment {...props.attributes}>{props.children}</Segment>,
+        paragraph: props => <Segment {...props.attributes} style={{ borderRadius: 0}}>{props.children}</Segment>,
+        section: props => <Segment {...props.attributes} style={{ borderRadius: 0}}>
+                            <Header as='h3' block {...props.attributes}>{props.children}</Header>
+                            {props.children}
+                          </Segment>, // A section always has a heading at the top
+        group: props => <Segment {...props.attributes} style={{ borderRadius: 0}}>{props.children}</Segment>,
       },
       marks: {
         bold: props => <strong>{props.children}</strong>,
@@ -117,6 +119,14 @@ class App extends React.Component {
       padding: 0,
     }
 
+    const popupStyle = {
+      borderRadius: 0,
+      opacity: 0.9,
+      padding: 20,
+    }
+
+    const popupSize = 'mini'
+
     return (
       <Container
         fluid
@@ -128,7 +138,7 @@ class App extends React.Component {
           direction='top' 
           visible={isVisible} 
           borderless
-          color='blue'
+          inverted
         >
           <Menu.Item name='home'>
             <Icon name='write' />
@@ -137,17 +147,171 @@ class App extends React.Component {
         </Sidebar>
         <div style={{ position: 'fixed', top: 60, left: 0}} id='structureMenu'>
           <List className='context-menu'>
-            <List.Item style={itemStyle} disabled={true}><div className='context-menu-item'><Icon name="arrow up" /></div></List.Item>
-            <List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="arrow down" /></div></List.Item>
-            <List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="hide" /></div></List.Item>
-            <List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="lock" /></div></List.Item>
-            <List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="object group" /></div></List.Item>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="arrow up" /></div></List.Item>}
+              content='Move up'
+              position='right center'
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle} />
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="arrow down" /></div></List.Item>}
+              content='Move down'
+              position='right center'
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle} />
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="indent" /></div></List.Item>}
+              content='Convert to section'
+              position='right center'
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle} />
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="list" style={{padding: 0}}/></div></List.Item>}
+              content='Convert to list'
+              position='right center'
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle} />
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="hide" /></div></List.Item>}
+              content='Hide'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>              
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="lock" /></div></List.Item>}
+              content='Lock'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>   
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="object group" /></div></List.Item>}
+              content='Group'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="exchange" /></div></List.Item>}
+              content='Switch visibility'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="clone" /></div></List.Item>}
+              content='Clone'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>                     
           </List>
         </div>
         <EditorContainer />
         <div style={{ position: 'fixed', top: 60, right: 0}} id='ContentMenu'>
           <List className='context-menu'>
-            <List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="history" /></div></List.Item>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="checkmark box" /></div></List.Item>}
+              content='Check grammar'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="idea" /></div></List.Item>}
+              content='Ideas'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="copy" /></div></List.Item>}
+              content='Copy as snippet'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>  
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="paste" /></div></List.Item>}
+              content='Paste snippet'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="comment" /></div></List.Item>}
+              content='Comments'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item'><Icon name="history" /></div></List.Item>}
+              content='History'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>
+          </List>
+        </div>
+        <div style={{ position: 'fixed', bottom: 0, right: 0}} id='ContentMenu'>
+          <List className='context-menu'>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item-layout'><Icon name="alarm mute" inverted /></div></List.Item>}
+              content='Distraction free'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item-layout'><Icon name="lightning" inverted /></div></List.Item>}
+              content='Quick preview'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
+          <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item-layout'><Icon name="external square" inverted /></div></List.Item>}
+              content='Export'
+              position='left center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
+          </List>
+        </div>
+        <div style={{ position: 'fixed', bottom: 0, left: 0}} id='ContentMenu'>
+          <List className='context-menu'>
+            <Popup
+              trigger={<List.Item style={itemStyle} disabled={false}><div className='context-menu-item-layout'><Icon name="help" inverted /></div></List.Item>}
+              content='Help'
+              position='right center' 
+              basic
+              inverted
+              size={popupSize}
+              style={popupStyle}/> 
           </List>
         </div>
       </Container>
